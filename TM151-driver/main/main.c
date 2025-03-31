be7c10d93cc1810e2ed9245571d71239208af060
Nelson Parra <nelson.parra@udea.edu.co>
2025-03-31 16:01:38 -0500

    solved uart problem

--- TM151-driver/main/main.c
+++ TM151-driver/main/main.c
@@ -123,9 +123,9 @@ void SerialPort_DataReceive(void){
 void app_main(void)
 {
    // Step 2: Initialization:
-	EasyProfile_C_Interface_Init();
+	//EasyProfile_C_Interface_Init();
 
-    if (uart_init_with_defaults(&myUART, 115200, 1024, UART_TX, UART_RX)) {
+    if (uart_init_with_defaults(&myUART, 115200, 1024, UART_TX, UART_RX) == 0) {
         printf("UART initialized successfully\n");
         
         const uint8_t data[] = "Hello, UART!";
@@ -134,27 +134,26 @@ void app_main(void)
         printf("UART initialization failed\n");
     }
 	
-    const uart_port_t uart_num = UART_NUM_2;
-    uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
-        .rx_flow_ctrl_thresh = 122,
-    };
-    // Configure UART parameters
-    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
+    // const uart_port_t uart_num = UART_NUM_2;
+    // uart_config_t uart_config = {
+    //     .baud_rate = 115200,
+    //     .data_bits = UART_DATA_8_BITS,
+    //     .parity = UART_PARITY_DISABLE,
+    //     .stop_bits = UART_STOP_BITS_1,
+    //     .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
+    //     .rx_flow_ctrl_thresh = 122,
+    // };
+    // // Configure UART parameters
+    // ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
 
-    // Set UART pins(TX: IO4, RX: IO5, RTS: IO18, CTS: IO19)
-    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, 4, 5, 18, 19));
+    // // Set UART pins(TX: IO4, RX: IO5, RTS: IO18, CTS: IO19)
+    // ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, 4, 5, 18, 19));
 
-    // Setup UART buffered IO with event queue
-    const int uart_buffer_size = (1024 * 2);
-    QueueHandle_t uart_queue;
-    // Install UART driver using an event queue here
-    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_2, uart_buffer_size, \
-                                            uart_buffer_size, 10, &uart_queue, 0));
+    // // Setup UART buffered IO with event queue
+    // const int uart_buffer_size = (1024 * 2);
+    // QueueHandle_t uart_queue;
+    // // Install UART driver using an event queue here
+    // ESP_ERROR_CHECK(uart_driver_install(UART_NUM_2, uart_buffer_size, uart_buffer_size, 10, &uart_queue, 0));
 
 	// while(1){
 		
